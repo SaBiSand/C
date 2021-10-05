9ef8377634a8585e272b3037a4239cedda45904c
Suseth Sandoval <91089461+SaBiSand@users.noreply.github.com>
2021-10-05 16:22:02 -0500

    Add files via upload

---
+++ Hola.c
@@ -0,0 +1,15 @@
+/*Hola mundo en C
+ * Aplicacion C
+ * Title		:Hola Mundo en C++
+ * Description		:Hola mundo en C desde vim en Windows
+ * Author		:Suseth Abigail Sandovval Damian
+ * Date			:202109
+ * Version		:1
+ * Usage		:c:\>gcc /path/Hola.c
+ */
+
+#include<stdio.h>
+int main(){
+	printf("Hola Mundo otr vez en C");
+	return 0;
+}
